dbdba42135e5f5a975ca21d8848105ad70616113
Johanna Rousi <johanna.rousi@gmail.com>
2019-05-08 15:06:36 +0300

    components for desktop

--- src/App.js
+++ src/App.js
@@ -3,17 +3,48 @@ import Header from './components/Header';
 import Searchbox from './components/Searchbox';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import HeaderDesktop from './components/HeaderDesktop';
+import FooterDesktop from './components/FooterDesktop';
+import MainDesktop from './components/MainDesktop';
 
 class App extends Component {
-    state = {};
+    state = {
+        width: window.innerWidth,
+    };
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    // make sure to remove the listener
+    // when the component is not mounted anymore
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
 
     render() {
+        const { width } = this.state;
+        const isMobile = width <= 500;
+
+        if (isMobile) {
+            return (
+                <div className="phone-screen">
+                    <Header />
+                    <Searchbox />
+                    <Main />
+                    <Footer />
+                </div>
+            );
+        }
         return (
-            <div className="phone-screen">
-                <Header />
-                <Searchbox />
-                <Main />
-                <Footer />
+            <div>
+                <HeaderDesktop />
+                <MainDesktop />
+                <FooterDesktop />
             </div>
         );
     }
---
+++ src/components/FooterDesktop.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FooterDesktop extends Component {
+    render() {
+        return (
+            <div>
+                <h1>FooterDesktop</h1>
+            </div>
+        );
+    }
+}
+
+export default FooterDesktop;
