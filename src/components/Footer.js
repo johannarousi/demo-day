93d257b9f785610023fd12da171061b6bf0be07f
StevePhuc <stevephuc.dev@gmail.com>
2019-05-15 10:59:17 +0300

    navlink over button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,29 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 const Footer = () => (
     <footer className="footer">
-        <button type="button" className="btn btn-word">
-            <i className="fab fa-wikipedia-w" />
-            <p className="font-title">
-                <NavLink to="/words">Words</NavLink>
-            </p>
-        </button>
+        <NavLink to="/words">
+            <button type="button" className="btn btn-word">
+                <i className="fab fa-wikipedia-w" />
+                <p className="font-title">Words</p>
+            </button>
+        </NavLink>
 
         <div className="verticalLine" />
-        <button type="button" className="btn btn-list">
-            <i className="fas fa-list" />
-            <p className="font-title">
-                <NavLink exact to="/">
-                    Search
-                </NavLink>
-            </p>
-        </button>
+        <NavLink to="/">
+            <button type="button" className="btn btn-list">
+                <i className="fas fa-list" />
+                <p className="font-title">Home</p>
+            </button>
+        </NavLink>
 
-        <button type="button" className="btn btn-list">
-            <i className="fas fa-list" />
-            <p className="font-title">
-                <NavLink to="/list">My List</NavLink>
-            </p>
-        </button>
+        <NavLink to="/list">
+            <button type="button" className="btn btn-list">
+                <i className="fas fa-list" />
+                <p className="font-title">My List</p>
+            </button>
+        </NavLink>
     </footer>
 );
 
