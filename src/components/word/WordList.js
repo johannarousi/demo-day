b33e8e915a458d15b8e0cdbb09bf80b9ade54dd1
Steve Phuc <stevephuc.dev@gmail.com>
2019-05-21 06:46:40 +0300

    add mylist and totallist

--- src/components/word/WordList.js
+++ src/components/word/WordList.js
@@ -6,39 +6,40 @@ export class WordList extends Component {
         word: '',
         frequency: '',
         subtitles: [],
-
-        datalist: [],
+        totalList: [],
+        myList: [],
     };
 
     componentDidMount() {
         /* check TotalList and MyList */
-
-        if (localStorage.getItem('totalData') === null) {
+        if (sessionStorage.getItem('totalList') === null) {
             const urlWordList = `https://yle-subtitle.herokuapp.com/api/list/100`;
             fetch(urlWordList)
                 .then(response => response.json())
                 .then(data => {
                     // console.log(data[0].frequency);
                     this.setState({
-                        datalist: data,
+                        totalList: data,
                     });
+                    sessionStorage.setItem('totalList', JSON.stringify(data));
                 });
         } else {
-            const totalData = JSON.parse(localStorage.getItem('totalData'));
-            this.setState({
-                datalist: totalData.totalList,
-            });
+            const totalList = JSON.parse(sessionStorage.getItem('totalList'));
+            this.setState({ totalList });
         }
-
-        /* variables total list , my list:
-        const totalList = totalData.totalList
-        const myList = totalData.myWordList */
+        // get mylist
+        if (localStorage.getItem('myList') === null) {
+            sessionStorage.setItem('myList', []);
+        } else {
+            const myList = JSON.parse(localStorage.getItem('myList'));
+            this.setState({ myList });
+        }
     }
 
     clickWord = e => {
         const clickWord = e.currentTarget.id;
 
-        const clickParent = e.currentTarget;
+        const clickParent = e.currentTarget.closest('.word_wrapper');
 
         const checkClick = clickParent.querySelector('.subtitle');
         const numSubtible = checkClick.textContent.length;
@@ -70,71 +71,53 @@ export class WordList extends Component {
     addWord = wordName => {
         console.log(wordName);
         // copying the datalist from state
-        const removeList = [...this.state.datalist];
-        // we need to get the index of the word we want to add/remove
-        let index;
-        for (let i = 0; i < removeList.length; i += 1) {
-            if (removeList[i].name === wordName) {
-                index = i;
-            }
-        }
-        console.log(index);
-        console.log(removeList);
-        const myListWord = removeList.splice(index, 1)[0];
-        console.log(myListWord);
-        console.log(removeList);
+        const { myList } = this.state;
+        const addList = [...myList, wordName];
         this.setState({
-            datalist: removeList,
+            myList: addList,
         });
-        // we want to store the removeList AND the myListWord so we create an object that we can store in the localStorage
-        if (localStorage.getItem('totalData') === null) {
-            const totalData = { totalList: removeList, myList: [myListWord] };
-            localStorage.setItem('totalData', JSON.stringify(totalData));
-            console.log(JSON.parse(localStorage.getItem('totalData')));
-        } else {
-            // we need the old list with the words in the mylist (because we don't want to override the old ones with the one new word)
-            // so we have to first take them from the localStorage and add the new word to them
-            const parsedData = JSON.parse(localStorage.getItem('totalData'));
-            const myNewList = parsedData.myList;
-            console.log(myNewList);
-            myNewList.push(myListWord);
-            console.log(myNewList);
-            const totalData = { totalList: removeList, myList: myNewList };
-
-            localStorage.setItem('totalData', JSON.stringify(totalData));
-            console.log(JSON.parse(localStorage.getItem('totalData')));
-        }
-
-        //     setState(datalist:removeList)
-        //     set LS toalta list = dataList
-        // add remov e Word to my List and save it
+        localStorage.setItem('myList', JSON.stringify(addList));
     };
 
     render() {
-        const { datalist } = this.state;
+        const { totalList, myList } = this.state;
         // console.log(datalist);
-        const subtitles = this.state.subtitles
-            .slice(0, 3)
-            .map(subtitle => (
-                <p style={{ fontStyle: 'italic' }} key={subtitle._id}>{`"${subtitle.subtitle}"`}</p>
-            ));
         const styles = {
             background: '#fff',
             width: '85%',
             margin: '3% auto',
             padding: '2% 0',
         };
-        const list = datalist.map(word => (
-            <div style={styles} key={word._id} onClick={this.clickWord} id={word.name}>
+        console.log(myList);
+        // console.log(totalList);
+        const showlist = [...totalList].filter(word => !myList.includes(word.name));
+        console.log(showlist);
+
+        const list = showlist.map(word => (
+            <div style={styles} key={word._id} className="word_wrapper">
                 <p>
                     Word <span>{word.name}</span> appears <span>{word.frequency}</span> times.
                 </p>
                 <div className="subtitle" />
-                <button onClick={() => this.addWord(word.name)}>Add</button>
+                <button onClick={() => this.addWord(word.name)} type="button">
+                    Add
+                </button>
+                <button onClick={this.clickWord} type="button" id={word.name}>
+                    More
+                </button>
             </div>
         ));
 
-        return <div>{list}</div>;
+        return (
+            <div>
+                <div>
+                    <h3>
+                        Process: {myList.length} /{totalList.length}
+                    </h3>
+                </div>
+                {list}
+            </div>
+        );
     }
 }
 
