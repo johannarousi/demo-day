afcc02e5c33c71df8dd516fea3c157f09cde9a5a
Dean Phan <dean.t.phan@gmail.com>
2019-05-31 11:45:23 +0300

    activeClassName

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+
+
 const Footer = () => (
     <footer className="footer">
-        <NavLink to="/words" className="btn btn-word">
+        <NavLink to="/words" className="btn btn-word" exact activeClassName="btn-active">
             <button type="button" className="btn-wrap">
                 <i className="fab fa-wikipedia-w" />
                 <p className="font-title">Words</p>
             </button>
         </NavLink>
 
-        <NavLink to="/" className="btn btn-search">
+        <NavLink to="/" className="btn btn-search" exact activeClassName="btn-active">
             <button type="button" className="btn-wrap">
                 <i className="fas fa-h-square" />
                 <p className="font-title">Home</p>
             </button>
         </NavLink>
 
-        <NavLink to="/list" className="btn btn-list">
+        <NavLink to="/list" className="btn btn-list" exact activeClassName="btn-active">
             <button type="button" className="btn-wrap">
                 <i className="fas fa-list" />
                 <p className="font-title">My List</p>
